34c9a77feb111f405fc7ad4bc47ed2d968a7e03e
Programming Hero <programming.hero1@gmail.com>
2024-02-05 13:19:57 -0600

    Implemented getRandom alphabet Function

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -10,7 +10,14 @@
 //     // console.log(playgroundSection.classList)
 // }
 
+function continueGame(){
+    // step -1: generate a random alphabet
+    const alphabet = getARandomAlphabet();
+    console.log('your random alphabet', alphabet);
+}
+
 function play(){
     hideElementById('home-screen');
     showElementById('play-ground');
+    continueGame()
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,9 +1,24 @@
-function hideElementById(elementId){
+function hideElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('hidden');
 }
 
-function showElementById(elementId){
+function showElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
+}
+
+function getARandomAlphabet() {
+    // get or create an alphabet array
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    // console.log(alphabets);
+
+    // get a random index between 0 -25
+    const randomNumber = Math.random() * 25;
+    const index = Math.round(randomNumber);
+    
+    const alphabet = alphabets[index];
+    // console.log(index, alphabet);
+    return alphabet;
 }
