012f00ede7c5b3e8e716dd4451f7302406c07636
Programming Hero <programming.hero1@gmail.com>
2024-02-05 16:57:45 -0600

    Get the Key pressed and check right or wrong key pressed

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -10,11 +10,26 @@
 //     // console.log(playgroundSection.classList)
 // }
 
-function handleKeyboardButtonPress(){
-    console.log('button pressed');
+function handleKeyboardKeyUpEvent(event){
+    const playerPressed = event.key;
+    console.log('player pressed', playerPressed);
+
+    // get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerPressed, expectedAlphabet);
+
+    // check matched or not
+    if(playerPressed === expectedAlphabet){
+        console.log('you get a point');
+    }
+    else {
+        console.log('you missed. you lost a life');
+    }
 }
-// capture keyboard key press
-document.addEventListener('keyup', handleKeyboardButtonPress)
+
+document.addEventListener('keyup', handleKeyboardKeyUpEvent);
 
 function continueGame(){
     // step -1: generate a random alphabet
