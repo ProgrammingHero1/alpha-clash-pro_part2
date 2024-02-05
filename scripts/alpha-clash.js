f303a8b8f1f5e1d409bbcda066805eec844122ce
Programming Hero <programming.hero1@gmail.com>
2024-02-05 09:53:25 -0600

    Change Home Screen to Playground by click

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,16 @@
+// function play(){
+//     // step-1: hide the home screen. to hide the screen add the class hidden to the home section
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+//     // console.log(homeSection.classList)
+
+//     // show the playground
+//     const playgroundSection = document.getElementById('play-ground');
+//     playgroundSection.classList.remove('hidden');
+//     // console.log(playgroundSection.classList)
+// }
+
 function play(){
-    console.log('play start now');
+    hideElementById('home-screen');
+    showElementById('play-ground');
 }
---
+++ scripts/utility.js
@@ -0,0 +1,9 @@
+function hideElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
+
+function showElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
