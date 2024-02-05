69a69457c01165a266c5fce828eb1a0c614e5bfa
Programming Hero <programming.hero1@gmail.com>
2024-02-05 13:38:19 -0600

    set alphabet background color

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -14,6 +14,13 @@ function continueGame(){
     // step -1: generate a random alphabet
     const alphabet = getARandomAlphabet();
     console.log('your random alphabet', alphabet);
+
+    // set randomly generated alphabet to the screen (show it)
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    // set background color
+    setBackgroundColorById(alphabet);
 }
 
 function play(){
--- scripts/utility.js
+++ scripts/utility.js
@@ -8,17 +8,34 @@ function showElementById(elementId) {
     element.classList.remove('hidden');
 }
 
-function getARandomAlphabet() {
-    // get or create an alphabet array
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+
+
+function getARandomAlphabet(){
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split('');
-    // console.log(alphabets);
 
-    // get a random index between 0 -25
-    const randomNumber = Math.random() * 25;
+    const randomNumber = Math.random()*25;
     const index = Math.round(randomNumber);
-    
+
     const alphabet = alphabets[index];
-    // console.log(index, alphabet);
     return alphabet;
 }
+
+// function getARandomAlphabet() {
+//     // get or create an alphabet array
+//     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+//     const alphabets = alphabetString.split('');
+//     // console.log(alphabets);
+
+//     // get a random index between 0 -25
+//     const randomNumber = Math.random() * 25;
+//     const index = Math.round(randomNumber);
+    
+//     const alphabet = alphabets[index];
+//     // console.log(index, alphabet);
+//     return alphabet;
+// }
