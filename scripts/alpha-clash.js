7969b8dd2774f0bbf92e60584b9a4f2a04750cd0
Programming Hero <programming.hero1@gmail.com>
2024-02-05 18:34:11 -0600

    Update Score and Life based on right or wrong key press

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -21,12 +21,35 @@ function handleKeyboardKeyUpEvent(event) {
     // check right or wrong key pressed
     if (playerPressed === expectedAlphabet) {
         console.log('you got a point!');
-        console.log('you have pressed correctly', expectedAlphabet);
+        // update score:
+        // 1. get the current score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+        console.log(currentScore);
+
+        // 2 .increase the score by 1
+        const newScore = currentScore + 1;
+
+        // 3. show the updated score
+        currentScoreElement.innerText = newScore;
+
+        // start a new round
         removeBackgroundColorById(expectedAlphabet);
         continueGame();
     }
     else {
-        console.log('dhurrr vaiya va apu...right key press koro')
+        console.log('dhurrr vaiya va apu...right key press koro');
+        // step-1: get the current Life number
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+        // step -2: reduce the life count
+        const newLife = currentLife - 1;
+
+        // step-3: display the updated life count
+        currentLifeElement.innerText = newLife;
     }
 }
 
